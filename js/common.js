f2ede940a375085e85a4983dd5bfc93cb748f7cf
junghye0128 <wjdgp1398@gmail.com>
2017-03-09 15:30:13 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,29 +7,35 @@ $(function(){
 
 	chart();
 
-	$('.work_list').css({'width':($(window).width())/2})
-	
-	$("#work li a").click(function(e){
-		e.preventDefault();
+	$(window).resize(function(){
 		var win_w = $(window).width();
-		var viewList = $(this).attr('date-list');
 
-		$('.work_list').addClass('on');
-
-		$('#wrap').animate({'left':-(win_w/2)});
-		$('.work_list div').find('.'+viewList).show()
-		$('.work_list').animate({'left':win_w/2});
-	})
-	
-	$(".work_list div > a").click(function(e){
-		e.preventDefault();
-		var win_w = $(window).width();
-		$('#wrap').animate({'left':0});
-		$('.work_list').animate({'left':win_w},function(){
+		$('.work_list').css({'width':($(window).width())/2})
+		$('#wrap').stop().animate({'left':0});
+		$('.work_list').stop().animate({'left':win_w},function(){
 			$('.work_list div ul').hide();
 			$('.work_list').removeClass('on');
 		});
-	})
+
+		$("#work li a").click(function(e){
+			e.preventDefault();
+			var viewList = $(this).attr('date-list');
+			$('.work_list div ul').hide();
+			$('.work_list').addClass('on');
+			$('#wrap').stop().animate({'left':-(win_w/2)});
+			$('.work_list div').find('.'+viewList).show()
+			$('.work_list').stop().animate({'left':win_w/2});
+		})
+		
+		$(".work_list div > a").click(function(e){
+			e.preventDefault();
+			$('#wrap').stop().animate({'left':0});
+			$('.work_list').stop().animate({'left':win_w},function(){
+				$('.work_list div ul').hide();
+				$('.work_list').removeClass('on');
+			});
+		})
+	}).resize()
 })
 
 
