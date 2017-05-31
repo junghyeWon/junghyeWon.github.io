2a5de327eec8073e8fe135dda1fae3a846c41085
junghye0128 <wjdgp1398@gmail.com>
2017-05-31 09:54:49 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,12 +7,13 @@ $(function(){
 			if(index == 1){
 				$('#fp-nav,#footer').hide();
 			}else if(index== 2){
+				$('#fp-nav,#footer').fadeIn();
 				$('.chart').easyPieChart({
 					barColor: '#fe5548',
 					trackColor: '#f0f0f0',
 					lineWidth: 8,
 					animate: 1500,
-					onStep: function(from, to, percent) {
+					onStep: function(from, to, percent){
 						$(this.el).find('.percent').text(Math.round(percent));
 					}
 				});
@@ -25,8 +26,9 @@ $(function(){
 	$(window).resize(function(){
 		var win_w = $(window).width();
 
-		$('.work_list').css({'width':($(window).width())/2})
-		$(".close").stop().fadeOut();
+		//reset
+		$('.work_list').css({'width':win_w/2})
+		$(".empty").stop().fadeOut();
 		$('#wrap').stop().animate({'left':0});
 		$('.work_list').stop().animate({'left':win_w},function(){
 			$('.work_list div ul').hide();
@@ -35,23 +37,19 @@ $(function(){
 
 		$("#work li a").click(function(e){
 			e.preventDefault();
-
-			$(".close").stop().fadeIn();
+			var viewList = $(this).data('list');
+			$(".empty").stop().fadeIn();
 			$(this).addClass('on');
-
-			var viewList = $(this).attr('date-list');
 			$('.work_list div ul').hide();
 			$('.work_list').addClass('on');
 			$('#wrap').stop().animate({'left':-(win_w/2)});
 			$('.work_list div').find('.'+viewList).show()
 			$('.work_list').stop().animate({'left':win_w/2});
 		})
-
-		$(".close").click(function(e){
+		$(".empty").click(function(e){
 			e.preventDefault();
-			$(".close").stop().fadeOut();
+			$(".empty").stop().fadeOut();
 			$("#work li a").removeClass('on');
-
 			$('#wrap').stop().animate({'left':0});
 			$('.work_list').stop().animate({'left':win_w},function(){
 				$('.work_list div ul').hide();
